f8bb2513d251e48042fd4484b90b75cb58502273
Adnan musil <adnan.museel123@gmail.com>
2021-02-23 14:23:18 +0500

    setting express pug and server configuration

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const app = express();
 const port = 8080;
 
 //express work
-app.use(express.static('static',options));
+app.use('/static',express.static('static'));
 app.use(express.urlencoded());
 
 
 //pug work
 app.set('view engine','pug');
 app.set('views',path.join(__dirname,'views'));
+
+//endpoint
+app.get('/',(req,res)=>{
+    const cont = 'dance website content';
+    const params = {'title':'dancing site','content':cont};
+    res.status(200).render('index.pug',params);
+})
+
+//server start
+app.listen(port,()=>{
+    console.log(`The website is started on port ${port}`);
+})
